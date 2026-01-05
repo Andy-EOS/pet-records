d1b89e1795bf5a1116f31273373fa1fb1d52cf1d
= <=>
2026-01-05 16:48:21 +0000

    Added email settings file.

---
+++ records/management/commands/email_details.py
@@ -0,0 +1,6 @@
+addresses = ['andy@theedgeofsanity.org.uk']
+gotify_token = 'AI9Gs.gxHyLnTAh'
+smtp_server = "smtp.protonmail.ch"
+port = 587
+sender_email = "pets@theedgeofsanity.org.uk"
+password = "QKGRCPW2VZRJ85YF"
