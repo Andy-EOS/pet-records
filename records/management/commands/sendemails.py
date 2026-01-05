4c8cdc7b6e1c210df654435135d51aa5bcf6b2d0
= <=>
2026-01-05 16:44:19 +0000

    Switched email sending script to use smtplib and ssl libraries instead of the django method as STARTTLS wouldn't work with the django method.

--- records/management/commands/sendemails.py
+++ records/management/commands/sendemails.py
@@ -1,17 +1,17 @@
 import requests
+import smtplib, ssl
 from django.core.management.base import BaseCommand, CommandError
 from records.models import Snake, Animal
-from django.core.mail import send_mail
 from datetime import date
-from records.management.commands.email_addresses import addresses as address_list
-from records.management.commands.email_addresses import gotify_token as gotify_token
+from records.management.commands.email_details import addresses as address_list
+from records.management.commands.email_details import gotify_token as gotify_token
+from records.management.commands.email_details import smtp_server as smtp_server
+from records.management.commands.email_details import port as port
+from records.management.commands.email_details import sender_email as sender_email
+from records.management.commands.email_details import password as password
 
 class Command(BaseCommand):
 
-    
-
-
-    
     def handle(self, *args, **options):
 
         animals = list(Snake.objects.all())
@@ -29,6 +29,14 @@ class Command(BaseCommand):
                 if (animal.get_spot_clean_due_in() < 2 and animal.get_spot_clean_due_in() >=0 ) or (animal.get_spot_clean_due_in()<0 and abs(animal.get_spot_clean_due_in())%7 ==0):
                     cleaning_text.append(f"{animal.animal_name} spot clean {animal.get_spot_clean_due()}")
 
+        if feeding_text or cleaning_text:
+            td = date.today()
+            d = str(td.day).rjust(2,'0')
+            m = str(td.month).rjust(2,'0')
+            y = str(td.year).rjust(4,'0')
+            feeding_due_date_text = f"{d}/{m}/{y}"
+
+        subject = f"Pet Reminders: {d}/{m}/{y}"
 
         email_body = ""
 
@@ -43,22 +51,14 @@ class Command(BaseCommand):
             for text in cleaning_text:
                 email_body = email_body + text + "\n"
 
-        if feeding_text or cleaning_text:
-            td = date.today()
-            d = str(td.day).rjust(2,'0')
-            m = str(td.month).rjust(2,'0')
-            y = str(td.year).rjust(4,'0')
-            feeding_due_date_text = f"{d}/{m}/{y}"
+            email_text = f"Subject:{subject}\n\n{email_body}"
 
-            subject = f"Pet Reminders: {d}/{m}/{y}"
-
-            send_mail(
-                subject,
-                email_body,
-                'pets@theedgeofsanity.org.uk',
-                address_list,
-                fail_silently=False,
-            )
+            context = ssl.create_default_context()
+            with smtplib.SMTP(smtp_server, port) as server:
+                server.starttls(context=context)
+                server.login(sender_email, password)
+                server.sendmail(sender_email, address_list, email_text)
+                server.quit()
 
             resp = requests.post(f'https://gotify.eos1.uk/message?token={gotify_token}', json={
             "message": email_body,
