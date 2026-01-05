2f4c3407a6d32b89fa49ad0094bf1e9cb907ee71
= <=>
2026-01-05 17:00:13 +0000

    -

--- records/management/commands/email_details.py
+++
@@ -1,6 +0,0 @@
-addresses = ['andy@theedgeofsanity.org.uk']
-gotify_token = 'AI9Gs.gxHyLnTAh'
-smtp_server = "smtp.protonmail.ch"
-port = 587
-sender_email = "pets@theedgeofsanity.org.uk"
-password = "8NRHXNFZ9P2A568D"
